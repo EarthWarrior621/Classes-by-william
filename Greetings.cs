e08be9eb22cb5c7b75f74f2dd02dd3c2a185b8ca
William Garcia <114087502+EarthWarrior621@users.noreply.github.com>
2023-04-13 09:46:52 -0500

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_by_william
+{
+    class Greetings
+    {
+
+        public void Welcome()
+        {
+            Console.WriteLine("Heloo Traveler, welcome!");
+        }
+
+
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes_by_william
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //
+            Greetings greetings = new Greetings();
+           
+            //
+            greetings.Welcome();
+           
+            //
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+           
+            //
+            greetings.Hello("William");
+        }
+    }
+}
